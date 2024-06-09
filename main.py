bfa6f547dc1ba67ed88aac43c745af6c01d7380f
KonochkinKN <awesome.kostyan@gmail.com>
2024-06-09 14:10:15 +0300

    убрал лишнее

--- main.py
+++ main.py
@@ -2,13 +2,5 @@ def example():
     print('Hello world')
 
 
-def greet():
-    print('hello')
-
-    
-def info():
-    print('doing come stuff')
-
-
 if __name__ == '__main__':
-    greet()
+    example()
