34f6fa061493013f6371e76acef8d5255dd62526
AliceBOOP <patriciadong70@gmail.com>
2024-06-11 20:58:59 +0300

    bruh

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 def example():
     print('Hello world')
 
-<<<<<<< HEAD
-=======
 def Disco():
     print('Detective Harrier')
     print('You are awful, detective')
->>>>>>> main
 
 
 if __name__ == '__main__':
